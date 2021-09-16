98dfcb8177d951a549c96c8a2c4604157f24f5d4
Ronik Bhaskar <ronik.r.bhaskar@gmail.com>
2021-09-15 21:59:23 -0500

    finally worked with decent image quality

--- fractals/helpers/ps_to_image.py
+++ fractals/helpers/ps_to_image.py
@@ -9,6 +9,7 @@ import glob
 import shutil
 from pathlib import Path
 import turtle
+from pdf2image import convert_from_path
 
 BASE_FOLDER = os.path.dirname(os.path.realpath(__file__))
 PARENT_FOLDER = os.path.dirname(BASE_FOLDER)
@@ -44,64 +45,77 @@ def convert_ps_to_pdf(ps_file : str) -> str:
 
     return ps_file.replace(PS_EXT, PDF_EXT)
 
-def cleanup_postscript_folder(postscript_folder : str) -> list:
+def cleanup_folder(folder : str, ext : str = PS_EXT) -> list:
     """
-    adds the postscript extension to all files with no extension
+    adds the extension to all files with no extension
     also ignores all other files
-    returns list of filenames with postscript extension
+    returns list of filenames with extension
     """
 
     # I really wanted to do something weird and hacky with
     # list comprehensions and boolean operation abuse.
     # but I decided against it.
-    ps_files = []
-    for file_name in os.listdir(postscript_folder):
-        name_no_ext, ext = os.path.splitext(file_name)
-        if ext not in {"", PS_EXT}:
+    files = []
+    for file_name in os.listdir(folder):
+        name_no_ext, temp_ext = os.path.splitext(file_name)
+        if temp_ext not in {"", ext} or \
+           os.path.isdir(f"{folder}/{file_name}"):
             continue
 
-        ps_file = f"{postscript_folder}/{name_no_ext}{PS_EXT}"
-        os.rename(f"{postscript_folder}/{file_name}", ps_file)
-        ps_files.append(ps_file)
+        current_file = f"{folder}/{name_no_ext}{ext}"
+        os.rename(f"{folder}/{file_name}", current_file)
+        files.append(current_file)
 
-    return ps_files
+    return files
 
-def generate_image_list(postscript_folder : str) -> list:
+def generate_image_list(folder : str, ext = PS_EXT) -> list:
     """
-    returns list of PIL Image objects from postscript files
+    returns list of PIL Image objects from image files
     sorts images by name
     """
 
-    return [Image.open(ps_file) for ps_file in 
-            sorted(cleanup_postscript_folder(postscript_folder))]
+    # edge case, requires extra library
+    if ext == PDF_EXT:
+        imagelist = [convert_from_path(file_name)[0] for file_name in 
+                sorted(cleanup_folder(folder, ext = ext))]
+        imagelist[-1].show()
+        return imagelist
+
+    return [Image.open(file_name) for file_name in 
+            sorted(cleanup_folder(folder, ext = ext))]
 
 
 
-def make_gif(draw : Callable, name : str, out_file : str) -> Image:
+def make_gif(draw : Callable, name : str, out_file : str):
     """
     combines images in postscript folder as frames of gif
     assumes images are sorted
     """
 
-    def save(interval : int, folder : str):
+    Path(TEMP_FOLDER).mkdir(parents = True, exist_ok = False)
+    drawing_is_running = True
+
+    def save(interval : int, folder : str, index : int):
         """saves turtle screen every interval as postscript file to folder"""
         if drawing_is_running:
-            turtle.getcanvas().postscript("{0}{1:04d}{2}".format(name, i, PS_EXT))
-            i += 1
-            turtle.ontimer(lambda: save(interval, folder), interval)
+            turtle.getcanvas().postscript(file = "{0}/{1}{2:04d}{3}".format(folder, name, index, PS_EXT))
+            index += 1
+            turtle.ontimer(lambda: save(interval, folder, index), interval)
 
-    drawing_is_running = True
-    i = 0
     interval = int(1000 / FPS)
-    ontimer(lambda: save(interval, TEMP_FOLDER), interval)
+    turtle.ontimer(lambda: save(interval, TEMP_FOLDER, 0), interval)
     draw()
     drawing_is_running = False
 
-    images = generate_image_list(TEMP_FOLDER)
+    convert_all_ps(TEMP_FOLDER, TEMP_FOLDER, filetype = "PDF")
+    print("if it got here, we didn't heck it up")
+
+    images = generate_image_list(TEMP_FOLDER, ext = PDF_EXT)
     images[0].save(out_file, 
                    save_all = True, 
                    append_images = images[1:], 
-                   loop = 0)
+                   loop = 0,
+                   duration = interval)
 
     shutil.rmtree(TEMP_FOLDER)
 
@@ -112,7 +126,9 @@ CONVERSION_FUNCTIONS = {
 
 def convert_all_ps(postscript_folder : str, image_folder : str, 
                    filetype : str = "png"):
-    """loops through all files in postscripts folder and converts them"""
+    """
+    loops through all files in postscripts folder and converts them
+    """
 
     filetype = filetype.lower()
 
@@ -121,10 +137,10 @@ def convert_all_ps(postscript_folder : str, image_folder : str,
 
     convert_ps = CONVERSION_FUNCTIONS[filetype]
 
-    Path(image_folder).mkdir(parents = False, exist_ok = True)
-
     image_paths = [convert_ps(ps_file) for ps_file in 
-                   cleanup_postscript_folder(postscript_folder)]
+                   cleanup_folder(postscript_folder)]
+
+    Path(image_folder).mkdir(parents = False, exist_ok = True)
 
     for file_path in image_paths:
         new_path = image_folder + "/" + os.path.basename(file_path)
--- fractals/sierpinski.py
+++ fractals/sierpinski.py
@@ -2,13 +2,21 @@
 Old recursive sierpinski triangle script I made.
 I decided it would fit with everything I was doing.
 Made some changes to turn it into a single function.
+TODO: Added a koch curve function.
 """
 
 from tkinter import *
 import turtle
+import os
+import shutil
+from typing import Callable
+from pathlib import Path
 from helpers.ps_to_image import make_gif
 
-SIDE_LENGTH = 300
+SIDE_LENGTH = 600
+FPS = 12
+BASE_FOLDER = os.path.dirname(os.path.realpath(__file__))
+TEMP_FOLDER = "pleaseignorethistempfolder"
 
 def sierpinski(side_length, pen):
     """
@@ -30,7 +38,7 @@ def sierpinski(side_length, pen):
     def _sierpinski(size):
         """simple recursive function to draw upside down triangles"""
 
-        if size < 3:
+        if size < side_length / 100:
             return
         pen.forward(size)
 
@@ -44,6 +52,36 @@ def sierpinski(side_length, pen):
 
     _sierpinski(side_length / 2)
 
+# def defunct_function(draw : Callable, name : str, out_file : str) -> Image:
+#     """
+#     combines images in postscript folder as frames of gif
+#     assumes images are sorted
+#     """
+
+#     Path(TEMP_FOLDER).mkdir(parents = True, exist_ok = False)
+#     drawing_is_running = True
+
+#     def save(interval : int, folder : str, index : int):
+#         """saves turtle screen every interval as postscript file to folder"""
+#         if drawing_is_running:
+#             print("{0}/{1}{2:04d}{3}".format(folder, name, index, PS_EXT))
+#             turtle.getcanvas().postscript(file="{0}/{1}{2:04d}{3}".format(folder, name, index, PS_EXT))
+#             index += 1
+#             turtle.ontimer(lambda: save(interval, folder, index), interval)
+
+#     interval = int(1000 / FPS)
+#     turtle.ontimer(lambda: save(interval, TEMP_FOLDER, 0), interval)
+#     draw()
+#     drawing_is_running = False
+
+#     images = generate_image_list(TEMP_FOLDER)
+#     images[0].save(out_file, 
+#                    save_all = True, 
+#                    append_images = images[1:], 
+#                    loop = 0)
+
+#     shutil.rmtree(TEMP_FOLDER)
+
 if __name__ == "__main__":
-    make_gif(lambda: sierpinski(SIDE_LENGTH, turtle.Turtle()), "test.gif")
+    make_gif(lambda: sierpinski(SIDE_LENGTH, turtle.Turtle()), "sierpinski", f"{BASE_FOLDER}/test8.gif")
     # sierpinski(SIDE_LENGTH, turtle.Turtle())
